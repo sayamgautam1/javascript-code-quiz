bafdaded2458ea884509d44ea9469d6912580c66
Sayam Gautam <sayamgautam1@gmail.com>
2022-03-29 13:45:43 +1030

    link javascript to the html file and get all the HTML elements

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// get all the elements from the html page to manipulate using JS
+
+let scoreCard = document.getElementById("scores");
+let timer = document.getElementById("timer");
+let quizContent = document.getElementById("quiz-content");
+let question = document.getElementById("questions");
+let answerOptions = document.getElementById("options");
+let result = document.getElementById("result");
