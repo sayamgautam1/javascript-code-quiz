62075098cd2624f5c649de4ef8466e5b578778de
Sayam Gautam <sayamgautam1@gmail.com>
2022-03-29 13:53:00 +1030

    add an array of object which represet the questions to be displayed

--- js/main.js
+++ js/main.js
@@ -6,3 +6,29 @@ let quizContent = document.getElementById("quiz-content");
 let question = document.getElementById("questions");
 let answerOptions = document.getElementById("options");
 let result = document.getElementById("result");
+
+//create an array of objects , each element representing a question to show inside the html document
+// q stands for question , o stands for option and a stands for answer
+//create 2 objects at first to test the working and add other later
+let questionChoices = [
+  {
+    q: "Inside the HTML document, where do you place your JavaScript code? ",
+    o: [
+      "Inside the <script> element",
+      "Inside the <head> element",
+      "Inside the <link> element",
+      "In the <footer> element",
+    ],
+    a: "Inside the <script> element",
+  },
+  {
+    q: "What operator is used to assign a value to a declared variable?",
+    o: [
+      "Question mark (?)",
+      "Equal sign (=)",
+      "Colon (:)",
+      "Double-equal (==)",
+    ],
+    a: "Equal sign (=)",
+  },
+];
